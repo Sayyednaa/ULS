70adc466efd0f3a94a0640f112d511550c388020
Sayyednaa <snawabaa@gmail.com>
2026-05-08 09:10:01 +0530

    added file size limit

--- fleetops_showcase/core/validators.py
+++ fleetops_showcase/core/validators.py
@@ -2,6 +2,12 @@ import os
 from django.core.exceptions import ValidationError
 
 def validate_file_extension(value):
+    # Size check (1MB Limit)
+    filesize = value.size
+    if filesize > 1048576:
+        raise ValidationError("The maximum file size that can be uploaded is 1MB")
+
+    # Extension check
     ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
     valid_extensions = ['.pdf', '.doc', '.docx', '.jpg', '.png', '.jpeg', '.xlsx', '.xls', '.csv']
     if not ext.lower() in valid_extensions:
